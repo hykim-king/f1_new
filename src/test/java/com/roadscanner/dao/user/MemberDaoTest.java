5efb107881e318b6353db11fd0c2dcd1dd49e636
현경 <chagusrud@gmail.com>
2023-08-11 14:50:56 +0900

    pom.xml - 주영님 마지막으로 커밋한 버전에서 업로드팀 커먼(파일업로드용), S3 의존성 추가(총 3개), 중복되는것 삭제 후 정리 root-context.xml - 주석처리 모두 삭제, 메일센더 + 기본 DB 데이터소스와 mybatis 매퍼 제외한 빈 생성객체 모두 삭제 MemberDaoTest - 로그인팀 브랜치와 맞춤. 그린라이트 확인. mybatis-config.xml 외 모든 삭제 파일 - 게시판팀 파일 삭제.(answerdao 오류 방지) **로그인팀, 업로드팀 서버 올라가는것, 서버 기능, 다오테스트 그린라이트 확인함.

--- src/test/java/com/roadscanner/dao/user/MemberDaoTest.java
+++ src/test/java/com/roadscanner/dao/user/MemberDaoTest.java
@@ -22,9 +22,8 @@ import com.roadscanner.domain.user.MemberVO;
 @RunWith(SpringJUnit4ClassRunner.class)												//스프링 테스트 컨텍스 프레임워크의 JUnit 확장 기능 지정
 @ContextConfiguration(locations = {
 		"file:src/main/webapp/WEB-INF/root-context.xml",
-		"file:src/main/resources/mybatis-config.xml"
-		})	//테스트 컨텍스트가 자동으로 만들어줄 applicationContext 위치
-@Transactional
+		"file:src/main/webapp/WEB-INF/servlet-context.xml"})	//테스트 컨텍스트가 자동으로 만들어줄 applicationContext 위치
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MemberDaoTest {
 	
 	@Autowired
