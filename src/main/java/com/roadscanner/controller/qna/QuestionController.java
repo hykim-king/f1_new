ee10b5fd6a32610543a767b2b47baf08e8781077
JYLee <sidd45454@gmail.com>
2023-08-24 12:10:12 +0900

    index, myquestion 관련 페이징, 검색 setting 메서드 추가

--- src/main/java/com/roadscanner/controller/qna/QuestionController.java
+++ src/main/java/com/roadscanner/controller/qna/QuestionController.java
@@ -23,56 +23,33 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
+    private static final int DEFAULT_PAGE = 1;
+    private static final int DEFAULT_SIZE = 10;
+
     private final QuestionService questionService;
     private final AnswerService answerService;
 
     @GetMapping
     public String index(Model model,
-                        @RequestParam(defaultValue = "1") int page,
-                        @RequestParam(defaultValue = "10") int size,
+                        @RequestParam(defaultValue = "" + DEFAULT_PAGE) int page,
+                        @RequestParam(defaultValue = "" + DEFAULT_SIZE) int size,
                         @RequestParam(defaultValue = "") String searchType,
                         @RequestParam(defaultValue = "") String keyword,
                         @RequestParam(required = false) Integer category,
                         @SessionAttribute("user") MemberVO memberVO) {
 
-        QuestionSearchCond searchCond = new QuestionSearchCond();
-        searchCond.setSearchType(searchType);
-        searchCond.setKeyword(keyword);
-        searchCond.setCategory(category); // 카테고리 설정
-
+        QuestionSearchCond searchCond = createSearchCond(searchType, keyword, category);
         int totalQuestions = questionService.countQuestions(searchCond);
-
-        // 페이지 번호가 유효한지 확인
-        if (page < 1) {
-            page = 1;
-        }
-
-        // 총 페이지 수가 0이면, 페이지 번호도 0으로 설정
-        if (totalQuestions == 0) {
-            page = 0;
-        }
-
-        PaginationDTO pagination = new PaginationDTO(page, size, totalQuestions);
-
-        // 현재 페이지 번호가 총 페이지 수보다 크면, 현재 페이지 번호를 총 페이지 수로 설정
-        if (page > pagination.getTotalPage()) {
-            page = pagination.getTotalPage();
-        }
+        PaginationDTO pagination = initializePagination(page, size, totalQuestions, model);
 
         model.addAttribute("questions", questionService.findAll(pagination, searchCond));
-        model.addAttribute("pagination", pagination);
-        model.addAttribute("page", page);
-
-        // 검색 조건 추가
         model.addAttribute("searchType", searchType);
         model.addAttribute("keyword", keyword);
         model.addAttribute("category", category);
 
-        // 공지사항 목록을 조회해서 모델에 추가
         List<QuestionListResponseDTO> notice = questionService.findNotice();
         model.addAttribute("notice", notice);
 
-        // user 등급 추가
         model.addAttribute("user", memberVO);
 
         return "qna/index";
@@ -80,53 +57,72 @@ public class QuestionController {
 
     @GetMapping("/my")
     public String findMyQuestion(Model model,
-                                 @RequestParam(defaultValue = "1") int page,
-                                 @RequestParam(defaultValue = "10") int size,
+                                 @RequestParam(defaultValue = "" + DEFAULT_PAGE) int page,
+                                 @RequestParam(defaultValue = "" + DEFAULT_SIZE) int size,
                                  @RequestParam(defaultValue = "") String searchType,
                                  @RequestParam(defaultValue = "") String keyword,
                                  @RequestParam(required = false) Integer category,
                                  @SessionAttribute("user") MemberVO memberVO) {
 
-        QuestionSearchCond searchCond = new QuestionSearchCond();
-
-        searchCond.setSearchType(searchType);
-        searchCond.setKeyword(keyword);
-        searchCond.setCategory(category); // 카테고리 설정
+        QuestionSearchCond searchCond = createSearchCond(searchType, keyword, category);
         searchCond.setId(memberVO.getId());  // user 정보 설정
 
         int totalQuestions = questionService.countMyQuestions(searchCond);
+        PaginationDTO pagination = initializePagination(page, size, totalQuestions, model);
 
-        // 페이지 번호가 유효한지 확인
+        // user 정보 추가
+        String id = memberVO.getId();
+
+        model.addAttribute("questions", questionService.findMyQuestion(id, pagination, searchCond));
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("category", category);
+
+        return "qna/my-question";
+    }
+
+    /**
+     * 검색 분류, 키워드, 카테고리 set을 위한 메서드
+     * @param searchType
+     * @param keyword
+     * @param category
+     * @return
+     */
+    private QuestionSearchCond createSearchCond(String searchType, String keyword, Integer category) {
+        QuestionSearchCond searchCond = new QuestionSearchCond();
+        searchCond.setSearchType(searchType);
+        searchCond.setKeyword(keyword);
+        searchCond.setCategory(category);
+        return searchCond;
+    }
+
+    /**
+     * 페이징 설정 메서드
+     * @param page
+     * @param size
+     * @param totalQuestions
+     * @param model
+     * @return
+     */
+    private PaginationDTO initializePagination(int page, int size, int totalQuestions, Model model) {
         if (page < 1) {
-            page = 1;
+            page = DEFAULT_PAGE;
         }
 
-        // 총 페이지 수가 0이면, 페이지 번호도 0으로 설정
         if (totalQuestions == 0) {
             page = 0;
         }
 
         PaginationDTO pagination = new PaginationDTO(page, size, totalQuestions);
 
-        // 현재 페이지 번호가 총 페이지 수보다 크면, 현재 페이지 번호를 총 페이지 수로 설정
         if (page > pagination.getTotalPage()) {
             page = pagination.getTotalPage();
         }
 
-        // user 정보 추가
-        String id = memberVO.getId();
-
-        model.addAttribute("questions", questionService.findMyQuestion(id, pagination, searchCond));
         model.addAttribute("pagination", pagination);
         model.addAttribute("page", page);
 
-        // 검색 조건 추가
-        model.addAttribute("searchType", searchType);
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("category", category);
-
-        return "qna/my-question";
-
+        return pagination;
     }
 
 
